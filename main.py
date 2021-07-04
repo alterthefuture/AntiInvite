630970cff554278052bcf168d3e6a9b785fdb5b9
lxy <80481433+lxy-7777@users.noreply.github.com>
2021-07-03 19:26:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ async def on_connect():
 
 @client.event
 async def on_message(message):
+    
+  ignore_member = ['your id','another id','another one'] #put your id and other people's id you want the bot to ignore
   ignore_channel = client.get_channel(idhere) #this is the channel where the bot ignore the discord invite link
-
-  if message.channel is ignore_channel:
+    
+  if message.author.id in ignore_member:
     pass
   else:
-    if "discord.gg/yourserver" in message.content: #replace yourserver with your invite link
-      pass
-    elif "discord.gg/" in message.content:
-      log_channel = client.get_channel(idhere) #this is the channel where the bot logs the message
+     if message.channel is ignore_channel:
+       pass
+     else:
+       if "discord.gg/yourserver" in message.content: #replace yourserver with your invite link
+         pass
+       elif "discord.gg/" in message.content:
+         log_channel = client.get_channel(idhere) #this is the channel where the bot logs the message
 
-      await message.delete()
-      await message.channel.send(f"**{message.author.mention} self promoting is not allowed.**")
+         await message.delete()
+         await message.channel.send(f"**{message.author.mention} self promoting is not allowed.**")
 
-      embed=discord.Embed(title=f"{message.author}",description=f"**Message sent by {message.author.mention} deleted in {message.channel.mention}**\n{message.content}\n\n **Reason**\n Invite link",color=discord.Color.red(),timestamp=message.created_at)
-      embed.set_footer(text=f"ID: {message.author.id}")
-      embed.set_thumbnail(url=message.author.avatar_url)
+         embed=discord.Embed(title=f"{message.author}",description=f"**Message sent by {message.author.mention} deleted in {message.channel.mention}**\n{message.content}\n\n **Reason**\n Invite link",color=discord.Color.red(),timestamp=message.created_at)
+         embed.set_footer(text=f"ID: {message.author.id}")
+         embed.set_thumbnail(url=message.author.avatar_url)
 
-      await log_channel.send(embed=embed)
+         await log_channel.send(embed=embed)
 
 client.run("token here")
